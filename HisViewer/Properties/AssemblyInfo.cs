fb9eaabf3ca1183c2bd3d45c32e8f85bca78e3db
Chris James <drummerboof@gmail.com>
2016-04-10 19:24:36 +0100

    Update assembly info

--- HisViewer/Properties/AssemblyInfo.cs
+++ HisViewer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HisViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Very simple HIS image viewer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("C James Solutions Limited")]
 [assembly: AssemblyProduct("HisViewer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
